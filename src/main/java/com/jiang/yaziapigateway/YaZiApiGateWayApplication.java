cad60ae586e72878ab8c7ec3f31a18b929651d7b
jiangyanming <1635156042@qq.com>
2024-05-18 19:39:35 +0800

    Initial commit

---
+++ src/main/java/com/jiang/yaziapigateway/YaZiApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.jiang.yaziapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaZiApiGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaZiApiGateWayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiang/yaziapigateway/YaZiApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiang.yaziapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaZiApiGateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
